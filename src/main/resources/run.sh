3d9de3b826121d5204d21f1c6aa077a3b07b12dc
fishitit <fishitit@qq.com>
2020-09-24 16:06:10 +0800

    更新启动脚本

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,6 +1,7 @@
 docker stop gupiao
 docker rm gupiao
-docker run --name gupiao \
+docker run -d  --name gupiao \
+	-v  /home/admin/docker/gupiao:/root/logs \
 	-v  /home/admin/docker/jenkins/jenkins_data/workspace/gupiao/target:/usr/src/ \
 	-w /usr/src \
 	java:8 \
